554295264bd176446ea297f92f0bc513fcff83ec
annngo850 <annn.go850@gmail.com>
2026-03-26 12:43:01 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const trainerRoutes = require('./routes/trainerr');
+const pokemonRoutes = require('./routes/pokemonr');
+const pokecenterRoutes = require('./routes/pokecenterr');
+const applicationRoutes = require('./routes/applicationr');
+
+app.use('/api/trainers', trainerRoutes);
+app.use('/api/pokemon', pokemonRoutes);
+app.use('/api/pokecenters', pokecenterRoutes);
+app.use('/api/applications', applicationRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT || 5000, () => {
+      console.log('Server running on port', process.env.PORT || 5000);
+    });
+  })
+  .catch(err => console.error(err));
